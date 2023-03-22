8c1cdb0fa67b2e8b45b8ecedc5f1e946e666de25
tienphampixelcent <tienpham@pixelcent.com>
2023-03-22 08:49:24 +0700

    update

--- src/modules/enterprise_dashboard/tour_on_sale/tour_on_sale.models.ts
+++ src/modules/enterprise_dashboard/tour_on_sale/tour_on_sale.models.ts
@@ -1,8 +1,16 @@
+interface price {
+  title: string;
+  minOld: number;
+  maxOld: number;
+  price: number;
+}
+
 export interface Create {
   tourId: number;
   discount: number;
   quantity: number;
   startDate: Date;
+  prices: price[];
 }
 
 export interface Update {
