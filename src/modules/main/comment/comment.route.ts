579c2aa6e2016cdf5299e76eaca72904bd10ca73
tienphampixelcent <tienpham@pixelcent.com>
2023-05-03 15:35:17 +0700

    get latest bill

--- src/modules/main/comment/comment.route.ts
+++ src/modules/main/comment/comment.route.ts
@@ -4,6 +4,6 @@ import { auth } from "middlewares";
 
 export const commentRouter = Router();
 
-commentRouter.route("/").get(Controller.findAll).post(auth, Controller.findAll);
+commentRouter.route("/").get(Controller.findAll).post(auth, Controller.create);
 commentRouter.route("/:id").put(auth, Controller.update).delete(auth, Controller.delete);
 commentRouter.route("/reply").post(auth, Controller.reply);
